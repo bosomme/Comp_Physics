95903db486fe70dd33b12eac8c82b7e333597791
bosomme <b.o.h.somme@fys.uio.no>
2017-12-01 20:27:41 +0100

    All methods seem to work correctly

--- Project_5/Project_5/main.cpp
+++ Project_5/Project_5/main.cpp
@@ -9,21 +9,30 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-
+#include <string>
 
 #include "lib.h"
 
 
+using namespace std;
+
+ofstream ofile;
+
+
+void print_to_file(string, double*, int, int);
 
 void Forward_Euler(int, double, double*, double*);
 void Backward_Euler(int, double, double*, double*);
 void Crank_Nicolson(int, double, double*);
 
+
 int main(int argc, const char * argv[]) {
+    string outfile_name;
+    
     int L = 1;
-    int x_steps = 100;
+    int x_steps = 10; //dx = 0.1
     int time_steps = 100;
-    double dx = (double)L/x_steps; double dt = 0.5*dx*dx;
+    double dx = (double)L/x_steps; double dt = 0.4*dx*dx;
     
     double alpha = dt/(dx*dx); //alpha = 0.5?
     
@@ -31,12 +40,38 @@ int main(int argc, const char * argv[]) {
     double *u_new = new double[x_steps];
     
     
-    for(int j=1; j<time_steps; j++){
-        // Forward_Euler(x_steps, alpha, u, u_new);
-        // Backward_Euler(x_steps, alpha, u, u_new);
-        Crank_Nicolson(x_steps, alpha, u);
-    }
+    // Decide method
+    cout << "Please give wanted method number:" << endl;
+    cout << "1 = Forward Euler, 2 = Backward Euler, 3 = Crank-Nicolson" << endl;
+    int method;
+    cin >> method;
     
+    for(int i=0; i<x_steps; i++) u[i] = u_new[i] = 0;
+    u[x_steps-1] = u_new[x_steps-1] = 1;
+    
+    
+    for(int j=1; j<time_steps; j++){
+        if (method == 1){
+            Forward_Euler(x_steps, alpha, u, u_new);
+            outfile_name = "Forward_Euler";
+        }
+        else if (method == 2){
+            Backward_Euler(x_steps, alpha, u, u_new);
+            outfile_name = "Backward_Euler";
+        }
+        else if (method == 3){
+            Crank_Nicolson(x_steps, alpha, u);
+            outfile_name = "Crank-Nicolson";
+        }
+        
+        // Print to file
+        if (j==0.5*time_steps){
+            print_to_file(outfile_name, u, x_steps, j);
+        }
+        else if (j==0.8*time_steps){
+            print_to_file(outfile_name, u, x_steps, j);
+        }
+    }
     
     
     delete [] u;
@@ -45,6 +80,23 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+
+
+void print_to_file(string outfile_name, double *u, int n, int j){
+    string outfile_name1 = outfile_name;
+    outfile_name1.append("_t="); outfile_name1.append(to_string(j));
+    outfile_name1.append(".txt");
+    ofile.open(outfile_name1);
+    ofile << setiosflags(ios::showpoint | ios::uppercase);
+    for(int i=0; i<n; i++){
+        ofile << i << "  ";
+        ofile << setprecision(8) << u[i] << endl;
+    }
+    ofile.close();
+}
+
+
+// Function that solves Au_new = u, with A being a tridiagonal matrix.
 void tridiag_solver(int n, double *a, double *b, double *c, double *u, double *u_new){
     double *b_tilde = new double[n];
     double *u_tilde = new double[n];
@@ -52,11 +104,9 @@ void tridiag_solver(int n, double *a, double *b, double *c, double *u, double *u
     b_tilde[0] = b[0];
     u_tilde[0] = u[0];
     
-    double temp = 0.0;
-    
+    double temp;
     // Forward substitution
-    for (int i=1; i<n; i++)
-    {
+    for (int i=1; i<n; i++){
         temp = a[i-1]/b_tilde[i-1];
         b_tilde[i] = b[i] - c[i-1]*temp;
         u_tilde[i] = u[i] - u_tilde[i-1]*temp;
@@ -65,14 +115,14 @@ void tridiag_solver(int n, double *a, double *b, double *c, double *u, double *u
     u_new[n-1] = u_tilde[n-1]/b_tilde[n-1];
     
     // Backward substitution
-    for (int i=n-2; i>=0; i--)
-    {
+    for (int i=n-2; i>=0; i--){
         u_new[i] = (u_tilde[i] - c[i]*u_new[i+1])/b_tilde[i];
     }
     
-    
-    delete [] b_tilde; delete [] u_tilde;
+    delete [] b_tilde;
+    delete [] u_tilde;
 }
+
 
 // Forward Euler Algorithm:
 // Function for computing one time step of PDE using the Forward Euler algorithm.
@@ -81,6 +131,10 @@ void Forward_Euler(int Number_of_XSteps, double alpha, double *u, double *u_new)
     for(int i=1; i<Number_of_XSteps; i++){
         u_new[i] = alpha*u[i-1] + (1-2*alpha)*u[i] + alpha*u[i+1];
     }
+    
+    // Boundary conditions
+    u_new[0] = 0; u_new[Number_of_XSteps-1] = 1;
+    
     // Update time solution with the new one
     for(int i=1; i<Number_of_XSteps; i++){
         u[i] = u_new[i];
@@ -93,19 +147,24 @@ void Backward_Euler(int Number_of_XSteps, double alpha, double *u, double *u_new
     double* a = new double[Number_of_XSteps];
     double* b = new double[Number_of_XSteps];
     // Fill arrays for diagonal and subdiagonal elements:
-    for(int i=1; i<Number_of_XSteps; i++){
-        a[i] = -alpha;      // Subdiagonal (same on each side
+    for(int i=0; i<Number_of_XSteps; i++){
+        a[i] = -alpha;      // Subdiagonal (same on each side)
         b[i] = 1 + 2*alpha; // Diagonal
     }
     
-    // Compute next time step:
+    u[Number_of_XSteps-1] += alpha;
+    
+    // Compute next time step
     tridiag_solver(Number_of_XSteps, a, b, a, u, u_new);
+    
+    // Boundary conditions
+    u_new[0] = 0;
+    u_new[Number_of_XSteps-1] = 1;
     
     // Update time solution with the new one
     for(int i=1; i<Number_of_XSteps; i++){
         u[i] = u_new[i];
     }
-    
     
     delete [] a; delete [] b;
 }
@@ -119,19 +178,23 @@ void Crank_Nicolson(int Number_of_XSteps, double alpha, double *u){
     for(int i=1; i<Number_of_XSteps; i++){
         utemp[i] = alpha*u[i-1] + (2-2*alpha)*u[i] + alpha*u[i+1];
     }
+    utemp[0] = 0; utemp[Number_of_XSteps-1] = 1;
     
     // Solving equation:
     double* a = new double[Number_of_XSteps];
     double* b = new double[Number_of_XSteps];
     // Fill arrays for diagonal and subdiagonal elements:
-    for(int i=1; i<Number_of_XSteps; i++){
-        a[i] = -alpha;      // Subdiagonal (same on each side
+    for(int i=0; i<Number_of_XSteps; i++){
+        a[i] = -alpha;      // Subdiagonal (same on each side)
         b[i] = 2 + 2*alpha; // Diagonal
     }
     
     // Compute next time step:
     tridiag_solver(Number_of_XSteps, a, b, a, utemp, u);
     
+    // Boundary conditions
+    u[0] = 0;
+    u[Number_of_XSteps-1] = 1;
     
     
     delete [] utemp; delete [] a; delete [] b;
